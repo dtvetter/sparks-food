d53e7f2ed67b20e0005d195641cd4ee52e2334e5
Matt Beall <mbeall@starverte.com>
2013-02-20 21:15:27 -0800

    Create food.php

---
+++ food.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Sparks Food
+Plugin URI: http://starverte.com/plugins/sparks-food
+Description: Part of the Sparks Framework. A plugin that allows for easy restaurant management.
+Version: alpha
+Author: Star Verte LLC
+Author URI: http://www.starverte.com
+License: GPLv2 or later
+
+    Copyright 2013  Star Verte LLC  (email : info@starverte.com)
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    
+    You should have received a copy of the GNU General Public License
+    along with Sparks Food.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+?>
